2ddab066a79157c5ea8f73b4e5b1e2344738a256
Daniel Byrne <dbyrne978@gmail.com>
2023-10-25 13:34:46 -0600

    Add grid to components

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,64 @@ import '@fontsource/inter';
 import BioCard from './components/BioCard.jsx'
 import ProjectCard from './components/ProjectCard';
 import { CssVarsProvider } from '@mui/joy';
+import Grid from '@mui/joy/Grid';
 
 function App() {
 
   return (
     <CssVarsProvider>
-      <BioCard />
-      <ProjectCard 
-        projectTitle = {"Etch-a-Sketch"}
-        mobileCompatibility = {false}
-        projectAddress = {'https://dbyrne978.github.io/etch-a-sketch/'}
-        projectImage = {"./selfie.jpg"}
-      />
-      <ProjectCard 
-        projectTitle = {"Rock, Paper, Scissors"}
-        mobileCompatibility = {true}
-        projectAddress = {'https://dbyrne978.github.io/rock-paper-scissors/'}
-        projectImage = {"./selfie.jpg"}
-      />
-      <ProjectCard 
-        projectTitle = {"Generic Landing Page"}
-        mobileCompatibility = {true}
-        projectAddress = {'https://dbyrne978.github.io/landing-page/'}
-        projectImage = {"./selfie.jpg"}
-      />
-      <ProjectCard 
-        projectTitle = {"Magic: the Gathering Blog"}
-        mobileCompatibility = {false}
-        projectAddress = {'https://dbyrne978.github.io/legacy-staples/'}
-        projectImage = {"./selfie.jpg"}
-      />
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+
+        <Grid
+          container
+          xs={12}
+          justifyContent="center"
+        >
+          <BioCard />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <ProjectCard 
+            projectTitle = {"Etch-a-Sketch"}
+            mobileCompatibility = {false}
+            projectAddress = {'https://dbyrne978.github.io/etch-a-sketch/'}
+            projectImage = {"./selfie.jpg"}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <ProjectCard 
+            projectTitle = {"Rock, Paper, Scissors"}
+            mobileCompatibility = {true}
+            projectAddress = {'https://dbyrne978.github.io/rock-paper-scissors/'}
+            projectImage = {"./selfie.jpg"}
+          />
+        </Grid>
+  
+        <Grid xs={12} sm={6} md={3}>
+          <ProjectCard 
+            projectTitle = {"Generic Landing Page"}
+            mobileCompatibility = {true}
+            projectAddress = {'https://dbyrne978.github.io/landing-page/'}
+            projectImage = {"./selfie.jpg"}
+          />
+        </Grid>
+  
+        <Grid xs={12} sm={6} md={3}>
+          <ProjectCard 
+            projectTitle = {"Magic: the Gathering Blog"}
+            mobileCompatibility = {false}
+            projectAddress = {'https://dbyrne978.github.io/legacy-staples/'}
+            projectImage = {"./selfie.jpg"}
+          />
+        </Grid>
+  
+      </Grid>
     </CssVarsProvider>
   )
 }

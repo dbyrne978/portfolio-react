58e6e77271bb367c0cfb51206495d7abaf171677
Daniel Byrne <dbyrne978@gmail.com>
2023-10-25 13:18:03 -0600

    Add all projects

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,25 @@ function App() {
       <ProjectCard 
         projectTitle = {"Etch-a-Sketch"}
         mobileCompatibility = {false}
-        projectAddress = {'http://www.danielbyrne.dev'}
+        projectAddress = {'https://dbyrne978.github.io/etch-a-sketch/'}
+        projectImage = {"./selfie.jpg"}
+      />
+      <ProjectCard 
+        projectTitle = {"Rock, Paper, Scissors"}
+        mobileCompatibility = {true}
+        projectAddress = {'https://dbyrne978.github.io/rock-paper-scissors/'}
+        projectImage = {"./selfie.jpg"}
+      />
+      <ProjectCard 
+        projectTitle = {"Generic Landing Page"}
+        mobileCompatibility = {true}
+        projectAddress = {'https://dbyrne978.github.io/landing-page/'}
+        projectImage = {"./selfie.jpg"}
+      />
+      <ProjectCard 
+        projectTitle = {"Magic: the Gathering Blog"}
+        mobileCompatibility = {false}
+        projectAddress = {'https://dbyrne978.github.io/legacy-staples/'}
         projectImage = {"./selfie.jpg"}
       />
     </CssVarsProvider>
